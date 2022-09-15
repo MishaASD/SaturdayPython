cbe5bad7fb828b591bad0f4645985d60e0abe6db
MishaASD <misha@asd.by>
2022-09-16 01:19:16 +0300

    Lesson 1 Homework

--- Homework_Lesson_1.py
+++ Homework_Lesson_1.py
@@ -1,15 +1,46 @@
 def find_digit():
     """
     Используя только математические операции, выделить из числа X N-ую цифру справа.
-    :return: Искомая цифра
     """
+    print("Выделить N-ую цифру справа")
+    x = int(input("Input X: "))
+    n = int(input("Input N: "))
+    answer = x // 10 ** (n-1) % 10
+    print(f'Your N number is {answer}')
 
 
 
 def exclude_digit():
+    """
+    Убрать в числе Х N-ую цифру справа
+    """
+    print("Убрать из числа N-ую цифру справа")
     x = int(input("Input X: "))
     n = int(input("Input N: "))
     answer = x//10**n*10**(n-1)+x%10**(n-1)
     print(f'Your new number is {answer}')
 
-exclude_digit()
+def help_the_rooster_hours():
+    """
+    Сколько часов осталось до восхода?
+    """
+    print("Сколько часов осталось до восхода?")
+    n = int(input("Текущий час: "))
+    r = int(input("Час восхода: "))
+    answer = (r + 24 - n)%24
+    print(f'Your new number is {answer}')
+
+
+def help_the_rooster_minutes():
+    """
+    Сколько минут осталось до восхода?
+    """
+    print("Сколько минут осталось до восхода?")
+    n = int(input("Текущий час: "))
+    nm = int(input("Текущие минуты: "))
+    r = int(input("Час восхода: "))
+    rm = int(input("Минуты восхода: "))
+    answer = (r*60 + rm + 1440 - n*60 - nm)%1440
+    print(f'Your new number is {answer}')
+
+help_the_rooster_minutes()
