36638070dfdf0c49e8fc40ab6a8035f5d400b077
MishaASD <misha@asd.by>
2022-09-30 23:49:34 +0300

    Lesson 3 Homework

--- Homework lesson 3.py
+++ Homework lesson 3.py
@@ -1,11 +1,15 @@
 def max_prime_nuber():
     n = int(input("введите икс: "))
     ans = n
+    answer = n
     l = 2
     while l <= n**0.5 and ans != 1:
         if ans % l == 0:
             ans = ans/l
+            print(f'{l=}')
+            answer = l
         else:
             l += 1
-    print(f"ваш ответ = {int(l)}")
+    print(f"ваш ответ = {int(answer)}")
+
 max_prime_nuber()
